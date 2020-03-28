455224b94a79e820d2a4e3a9cb7dd51931541fec
Vandemberg Lima <vandemberg.silva.lima@gmail.com>
2020-03-28 13:30:15 -0300

    removing database from the cache

--- backend/tests/integration/api/ong.spec.js
+++ backend/tests/integration/api/ong.spec.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../../src/app');
+const app = require('../../../src/app');
 
 const connection = require('./../../../src/database/connection');
 
